e78178c95e2f91863bd4c45bf4a82e98a3941822
Akhilesh002 <skakhilesh0017@gmail.com>
2020-11-26 18:24:58 +0530

    both dice change their face if click any one dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +18,62 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber, rightDiceNumber;
+  int count = 0;
+  String dice1, dice2;
+
+  random() {
+    if (count == 0) {
+      leftDiceNumber = 1;
+      rightDiceNumber = 1;
+      dice1 = "images/dice$leftDiceNumber.png";
+      dice2 = "images/dice$rightDiceNumber.png";
+    } else {
+      var rng = new Random();
+      leftDiceNumber = rng.nextInt(6) + 1;
+      rightDiceNumber = rng.nextInt(6) + 1;
+    }
+    count++;
+  }
+
+  changeDiceState() {
+    setState(() {
+      dice1 = "images/dice$leftDiceNumber.png";
+      dice2 = "images/dice$rightDiceNumber.png";
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    random();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                random();
+                changeDiceState();
+              },
+              child: Image.asset(dice1),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                random();
+                changeDiceState();
+              },
+              child: Image.asset(dice2),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
